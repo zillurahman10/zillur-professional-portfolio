6997c2858808c75cf21bfc231e9a9ac761b8dbbf
zillurahman10 <zillur.hero40@gmail.com>
2023-07-04 12:11:00 +0600

    About me section [Done]

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import AboutMe from './Sections/AboutMe/AboutMe';
 import Banner from './Sections/Banner/Banner';
 
 function App() {
   return (
     <div className='App'>
       <Banner></Banner>
+      <AboutMe></AboutMe>
     </div>
   );
 }
--- src/Sections/AboutMe/AboutMe.jsx
+++ src/Sections/AboutMe/AboutMe.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import "./AboutMe.css";
+import AboutMePhoto from "../../Assets/AboutMe.jpg";
 
 function AboutMe() {
-  return <div></div>;
+  return (
+    <div className="about-main">
+      <h1 className="about-heading">About Me</h1>
+      <div className="banner">
+        <img src={AboutMePhoto} alt="" className="banner-photo"></img>
+        <div>
+          <h1 className="about-text">
+            I'm a professional Full-Stack and Also a MERN-Stack Web Developer. I
+            develop web application with my core skills. I seek everyday to gain
+            new skills that are useful in my practical life.
+          </h1>
+          <button className="banner-btn">
+            <a
+              target="_blank"
+              href="https://drive.google.com/file/d/1MRq9uhw9puwKtw-X2tlXzYXLmRZ2gADW/view?usp=sharing"
+              rel="noreferrer"
+            >
+              Hire Me
+            </a>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default AboutMe;
